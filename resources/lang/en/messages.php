edb06b7339ed2dcc1f850250292af4245cbc7bbc
Sergio Lobaton <sergio.lobaton@endeandina.bo>
2018-10-27 18:42:41 -0400

    add internationalization for login

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -1,9 +1,11 @@
 <?php
 return [
     'login' => [
-        'email' => 'E-Mail Address',
-        'password' => 'Password',
-        'remember_me' => 'Remember Me',
+        'email'          => 'E-Mail Address',
+        'password'       => 'Password',
+        'remember_me'    => 'Remember Me',
+        'reset_password' => 'Forgot your password?'
+    ],
 
-    ]
+    'logout' => 'Logout',
 ];
---
+++ resources/lang/es/messages.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'login' => [
+        'email'          => 'Correo electrónico',
+        'password'       => 'Contraseña',
+        'remember_me'    => 'Recordarme',
+        'reset_password' => 'Olvidaste tu contraseña?'
+    ],
+
+    'logout' => 'Salir',
+];
